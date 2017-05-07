d3b4fb6db91abd9ff8f4c7021371a070c19a5b77
bendavidelad <elad.bendavid1@mail.huji.ac.il>
2017-05-07 21:42:59 +0300

    asfd

---
+++ MapReduceSearch.h
@@ -0,0 +1,26 @@
+#ifndef OSTARGIL3_MAPREDUCESEARCH_H
+#define OSTARGIL3_MAPREDUCESEARCH_H
+
+#include <iostream>
+#include <dirent.h>
+#include <stdlib.h>
+#include <libltdl/lt_system.h>
+#include <list>
+
+#include "MapReduceClient.h"
+#include "MapReduceFramework.h"
+#include "MapReduceClientUser.h"
+
+class MapReduceSearch : MapReduceBase{
+
+    MapReduceSearch();
+
+    void Map(const k1Base *const key, const v1Base *const val);
+
+
+    virtual void Reduce(const k2Base *const key, const V2_VEC &vals);
+
+
+};
+
+#endif //OSTARGIL3_MAPREDUCESEARCH_H
