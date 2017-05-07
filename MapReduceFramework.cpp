314147eacb63e538e4d521d914397ea6211423d2
bendavidelad <elad.bendavid1@mail.huji.ac.il>
2017-05-07 22:04:16 +0300

    asd

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -4,21 +4,10 @@
 
 
 
-class MapReduceFramewrok
-{
 
-    OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase &mapReduce, IN_ITEMS_VEC &
-    itemsVec, int multiThreadLevel, bool autoDeleteV2K2){
+OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase &mapReduce, IN_ITEMS_VEC &
+    itemsVec, int multiThreadLevel, bool autoDeleteV2K2){}
 
-    }
+void Emit2 (k2Base*, v2Base*){}
 
-
-    void Emit2 (k2Base*, v2Base*){}
-    void Emit3 (k3Base*, v3Base*){}
-
-
-
-
-
-
-};
+void Emit3 (k3Base*, v3Base*){}
