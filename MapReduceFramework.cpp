b4f04805261bbe911cb54a3dcee6b6cba3683a9e
bendavidelad <elad.bendavid1@mail.huji.ac.il>
2017-05-28 21:23:14 +0300

    ss

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -26,6 +26,7 @@ std::vector<pthread_t> threadsGlobal(0);
 unordered_map<pthread_t , listOfPairsK2BaseV2Base*> containerMapGlobal;
 
 bool isFinal = false;
+bool isJoin = false;
 //########################################################################
 // Semaphores
 const int semaphoreShuffleInt = 0;
@@ -36,7 +37,7 @@ const int WORK_BETWEEN_THE_PROCESSES = 0;
 //  Mutex
 pthread_mutex_t mutexItemsVec = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutexThreadCreation = PTHREAD_MUTEX_INITIALIZER;
-unordered_map<pthread_t, listOfPairsK2BaseV2Base*> mutexMapGlobal;
+unordered_map<pthread_t, pthread_mutex_t> mutexMapGlobal;
 
 
 
@@ -74,6 +75,37 @@ IN_ITEMS_VEC* getChunkOfPairs(){
         return nullptr;
     }
 }
+
+IN_ITEMS_VEC* getChunkOfPairsAfterShuffle(){
+    unsigned long chunkSize = KEYS_PER_THREAD;
+    if (itemsVecPlace > 0){
+        // Critical Section!!!
+        pthread_mutex_lock(&mutexItemsVec);
+        unsigned long start = itemsVecPlace;
+        itemsVecPlace -= KEYS_PER_THREAD;
+        pthread_mutex_unlock(&mutexItemsVec);
+        // End of Critical Section!!!
+        // check if there's less threads than KEYS_PER_THREAD
+        if (start < KEYS_PER_THREAD){
+            chunkSize = start;
+        }
+        IN_ITEMS_VEC::const_iterator first = itemsVecGlobal.begin() + itemsVecPlace - chunkSize ;
+        IN_ITEMS_VEC::const_iterator last =  itemsVecGlobal.end()+ itemsVecPlace;
+        vector<IN_ITEM>* newVec;
+        try{
+            newVec = new vector<IN_ITEM>(first, last);
+        }catch(const std::bad_alloc&){
+            cout<<BAD_ALLOC_MSG<<endl;
+            exit(EXIT_FAILURE);
+        }
+        return newVec;
+    } else {
+
+        return nullptr;
+    }
+}
+
+
 /**
  *
  * @return
@@ -84,6 +116,8 @@ void* execMap(void*)
     //lock(x) ->here all the threads wait for the main thread to finish making all the threads
     //unlock(x)-> and now after we past this phase ot mean no one locked x
     pthread_mutex_unlock(&mutexThreadCreation);
+
+
     while (true)
     {
         IN_ITEMS_VEC *currVec = getChunkOfPairs();
@@ -95,6 +129,7 @@ void* execMap(void*)
         }
         for (int i = 0; i < currVec->size(); i++)
         {
+
             mapReduceGlobal->Map(((*currVec)[i]).first, ((*currVec)[i]).second); // might not
             // work TODO
         }
@@ -102,23 +137,53 @@ void* execMap(void*)
     pthread_exit(NULL); //TODO might gonna need to check
 }
 
+
+void* execReduce(void*)
+{
+    pthread_mutex_lock(&mutexThreadCreation);
+    //lock(x) ->here all the threads wait for the main thread to finish making all the threads
+    //unlock(x)-> and now after we past this phase ot mean no one locked x
+    pthread_mutex_unlock(&mutexThreadCreation);
+
+
+    while (true)
+    {
+        IN_ITEMS_VEC *currVec = getChunkOfPairsAfterShuffle();
+        if (currVec == nullptr)
+        {
+            isFinal = true;
+            sem_post(&semaphoreShuffle);
+            break;
+        }
+        for (int i = 0; i < currVec->size(); i++)
+        {
+
+            mapReduceGlobal->Map(((*currVec)[i]).first, ((*currVec)[i]).second); // might not
+            // work TODO
+        }
+    }
+    pthread_exit(NULL); //TODO might gonna need to check
+}
+
+
+
 /**
  *.
  * @return
  */
-void* shuffle(void*) {
+void* shuffle(void*)
+{
     std::map<k2Base,std::list<v2Base>> shuffleMap;
-
-
-    while(!isFinal)
+    while(!isJoin)
     {
         for (unsigned long i = 0; i < threadsGlobal.size(); i++)
         {
             if ((containerMapGlobal[threadsGlobal[i]]->size() > 0))
             {
-
                 pair<k2Base*, v2Base*> currPair = containerMapGlobal.at(threadsGlobal[i])->back();
-
+                pthread_t currThreadID  = threadsGlobal[i];
+                //locking the critical code section-> the mutual resource
+                pthread_mutex_lock(&mutexMapGlobal[currThreadID]);
                 containerMapGlobal.at(threadsGlobal[i])->pop_back();
                 if (shuffleMap.count(*currPair.first))
                 {
@@ -131,28 +196,38 @@ void* shuffle(void*) {
                     std::pair<k2Base, std::list<v2Base>> newPair = make_pair(currPair.first, listV2Base);
                     shuffleMap.insert(newPair);
                 }
+                //unlock the mutex
+                pthread_mutex_unlock(&mutexMapGlobal[currThreadID]);
                 sem_wait(&semaphoreShuffle);
                 break;
             }
         }
     }
-    for(int j = 0; j< multiThreadLevelGlobal ;++j)
+    //after the join of the threads
+    for (unsigned long k = 0; k < threadsGlobal.size(); k++)
     {
-        int rc;
-        void* status;
-        rc = pthread_join(threadsGlobal[j], &status);
-        if(rc)
+        if ((containerMapGlobal[threadsGlobal[k]]->size() > 0))
         {
-            cerr<<"error"<<rc<<endl;//TODO make a standard error
+            while (containerMapGlobal[threadsGlobal[k]]->size() != 0)
+            {
+                pair<k2Base*, v2Base*> currPair = containerMapGlobal.at(threadsGlobal[k])->back();
+                containerMapGlobal.at(threadsGlobal[k])->pop_back();
+                if (shuffleMap.count(*currPair.first))
+                {
+                    shuffleMap.at(*currPair.first).push_back(*currPair.second);
+                }
+                else
+                {
+                    std::list<v2Base> *listV2Base = new list<v2Base>();
+                    listV2Base->push_back(*currPair.second);
+                    std::pair<k2Base, std::list<v2Base>> newPair = make_pair(currPair.first, listV2Base);
+                    shuffleMap.insert(newPair);
+                }
+                sem_wait(&semaphoreShuffle);
+            }
         }
     }
-    //all the treads
-    while ()
-
-
-
-
-
+    pthread_exit(NULL); //TODO might gonna need to check
 }
 
 
@@ -180,7 +255,8 @@ OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase &mapReduce, IN_ITEMS_VEC &
 
     //mutex lock(x)-> so we connect between thread id and the container
     pthread_mutex_lock(&mutexThreadCreation);
-    for(i = 0 ; i < multiThreadLevel ; i++){
+    for(i = 0 ; i < multiThreadLevel ; i++)
+    {
         threadCreation = pthread_create(&threadsGlobal[i] , NULL , execMap , NULL);
         listOfPairsK2BaseV2Base *currContainer;
         try {
@@ -188,11 +264,15 @@ OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase &mapReduce, IN_ITEMS_VEC &
         }catch (const std::bad_alloc&){
             exit(EXIT_FAILURE);
         }
-        std::pair<pthread_t, listOfPairsK2BaseV2Base*> currPair = make_pair(threadsGlobal[i],
+        std::pair<pthread_t, listOfPairsK2BaseV2Base*> pairToThreadContainer = make_pair(threadsGlobal[i],
                                                                    currContainer);
-        containerMapGlobal.insert(currPair);
-        pthread_mutex_t threadsMutex[] = PTHREAD_MUTEX_INITIALIZER;
-
+        containerMapGlobal.insert(pairToThreadContainer);
+        //init the mutex pf the thread
+        pthread_mutex_t thread_mutex;
+        pthread_mutex_init(&thread_mutex, NULL);
+        //make the pair for the mutex map that
+        std::pair<pthread_t, pthread_mutex_t> pairToMutexMap = make_pair(threadsGlobal[i], thread_mutex);
+        mutexMapGlobal.insert(pairToMutexMap);
         if (threadCreation){
             cout << "Error:unable to create thread," << threadCreation << endl;
             exit(EXIT_FAILURE);
@@ -207,6 +287,22 @@ OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase &mapReduce, IN_ITEMS_VEC &
     threadCreation = pthread_create(&threadsGlobal[i] , NULL , shuffle , NULL);
 
 
+    for(int j = 0; j< multiThreadLevelGlobal ;++j)
+    {
+        int rc;
+        void* status;
+        rc = pthread_join(threadsGlobal[j], &status);
+        if(rc)
+        {
+            cerr<<"error"<<rc<<endl;//TODO make a standard error
+        }
+    }
+    isJoin = true;
+
+    //all the treads definitely have finished
+
+
+
 
 
 }
@@ -214,10 +310,15 @@ OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase &mapReduce, IN_ITEMS_VEC &
 
 
 void Emit2 (k2Base* k2, v2Base* v2){
+
     pthread_t currThreadID  = pthread_self();
     listOfPairsK2BaseV2Base *currContainer =  containerMapGlobal.at(currThreadID);
     std::pair<k2Base* , v2Base*> currPair = make_pair(k2 , v2);
+    //locking the critical code section-> the mutual resource
+    pthread_mutex_lock(&mutexMapGlobal[currThreadID]);
     currContainer->push_back(currPair);
+    //unlock the mutex
+    pthread_mutex_unlock(&mutexMapGlobal[currThreadID]);
     sem_post(&semaphoreShuffle);
 }
 
